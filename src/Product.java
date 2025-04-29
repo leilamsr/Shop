7496f1f0dea54e1433acc44f035e914f217b7891
leila <leila.m2006131@gmail.com>
2025-04-29 21:06:49 +0330

    add Product methods & filds

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,41 @@
 public abstract class Product {
+    private String title;
+    private Double price;
+    private String id;
+
+    public Product(String title, Double price, String id) {
+        this.title = title;
+        this.price = price;
+        this.id = generateId();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String toString() {
+        return title + "\t" + price + "\t" + id;
+    }
+
+    protected abstract String generateId();
 }
