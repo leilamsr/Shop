ff06f3fa29a77fcf365154c8e7a3120ccd343c30
leila <leila.m2006131@gmail.com>
2025-04-29 21:55:02 +0330

    add Notebook methods & filds

--- src/Notebook.java
+++ src/Notebook.java
@@ -1,2 +1,38 @@
 public class Notebook extends Product {
+    private int pageCount;
+    private boolean isHardCover;
+
+    private static int count = 2;
+
+    public Notebook(String title, double price, int pageCount, boolean isHardCover) {
+        super(title, price);
+        this.pageCount = pageCount;
+        this.isHardCover = isHardCover;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public boolean isHardCover() {
+        return isHardCover;
+    }
+
+    public void setHardCover(boolean hardCover) {
+        isHardCover = hardCover;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + pageCount + "\t" + isHardCover;
+    }
+
+    @Override
+    protected String generateId() {
+        return "Notebook " + count++;
+    }
 }
