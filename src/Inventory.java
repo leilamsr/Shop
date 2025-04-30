ffc0be2960ca2768b7fa98e9d9e90e67702ba31a
leila <leila.m2006131@gmail.com>
2025-04-30 12:28:49 +0330

    add Inventory methods & filds

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,2 +1,50 @@
+import java.util.ArrayList;
+
 public class Inventory<T extends Product> {
+    private ArrayList<T> items = new ArrayList<>();
+
+    public ArrayList<T> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<T> items) {
+        this.items = items;
+    }
+
+    public void applyDiscount (String productName, int discount) {
+        for (T item : items) {
+            if (item.getTitle().equals(productName)) {
+                double price = item.getPrice() - (item.getPrice() * discount / 100.0);
+                item.setPrice(price);
+            }
+        }
+    }
+
+    public void addItems (T product) {
+        items.add(product);
+    }
+
+    public void removeItemsById(int id) {
+        String delete = " " + id;
+        items.removeIf(item -> item.getTitle().equals(delete));
+    }
+
+    public T findItemsById(int id) {
+        String find = " " + id;
+        for (T item : items) {
+            if (item.getTitle().equals(find)) {
+                return item;
+            }
+            else {
+                System.out.println("Not found");
+            }
+        }
+        return null;
+    }
+
+    public void displayAll() {
+        for (T item : items) {
+            System.out.println(item);
+        }
+    }
 }
